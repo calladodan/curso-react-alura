6b6c295dbd63ff645990eff41a51a9ce5a567a1f
Daniel Callado <dcallado@id.uff.br>
2020-04-16 13:16:12 -0300

    Aula 02 - Iniciando tabela estática

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Produto</th>
+            <th>Preço</th>
+            <th>Remover</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Daniel</td>
+            <td>React</td>
+            <td>10</td>
+            <td><button>Remover</button></td>
+          </tr>
+          <tr>
+            <td>Pablo</td>
+            <td>Java</td>
+            <td>20</td>
+            <td><button>Remover</button></td>
+          </tr>
+          <tr>
+            <td>Paulo</td>
+            <td>Horroroso</td>
+            <td>1000</td>
+            <td><button>Remover</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
